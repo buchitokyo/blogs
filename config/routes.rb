096cbb1efef02072ed271e6c8b43f86d770cf8cf
Hirotoshi Kawabuchi <hirotoshi.kawabuchi@gmail.com>
2018-06-20 14:18:55 +0000

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
- root to: 'tops#index'
+ 
   resources :blogs do
   collection do
       post :confirm
   end
   end
+  
+  root to: 'tops#index'
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end  
