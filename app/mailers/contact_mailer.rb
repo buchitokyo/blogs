8de72e8631948fbcd94c3a55231eaeb2ca126a47
hirotoshi kawabuchi <hirotoshi.kawabuchi@gmail.com>
2018-07-29 20:19:10 +0900

    修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(blog)
     @blog = blog
-  @contact_user = @blog.user.email
+    @contact_user = @blog.user.email
     mail to: @contact_user, subject: "ブログの完了確認メール"
   end
 end
